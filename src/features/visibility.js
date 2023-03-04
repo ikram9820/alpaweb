895a2ee5156b4ce79251f705fdf583af94a8876f
unknown <ikram9820khan@gmail.com>
2023-03-04 12:14:04 +0500

    visibility feature added

---
+++ src/features/visibility.js
@@ -0,0 +1,67 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { apiCallBegan } from "./api";
+
+const slice = createSlice({
+  name: "visibility",
+  initialState: {
+    visibility: {},
+    isLoading: false,
+    lastFetch: null,
+  },
+  reducers: {
+    apiRequested: (state, action) => {
+      state.isLoading = true;
+    },
+    apiRequestFailed: (state, action) => {
+      state.isLoading = false;
+    },
+
+    visibilityReceived: (state, action) => {
+      state.visibility = action.payload;
+      state.isLoading = false;
+    },
+
+    visibilityCreated: (state, action) => {
+      state.visibility = action.payload;
+      state.isLoading = false;
+    },
+    visibilityUpdated: (state, action) => {
+      state.visibility = action.payload;
+      state.isLoading = false;
+    },
+  },
+});
+
+export const {
+  visibilityCreated,
+  visibilityUpdated,
+  visibilityReceived,
+  apiRequested,
+  apiRequestFailed,
+} = slice.actions;
+export default slice.reducer;
+
+// Action Creators
+const url = "/visibility";
+
+
+export const createVisibility = (visibility) =>
+  apiCallBegan({
+    url,
+    method: "post",
+    data: visibility,
+    onSuccess: visibilityCreated.type,
+  });
+
+export const updateVisibility = (visibility) =>
+  apiCallBegan({
+    // /visibility
+    //PUT /visibility/1
+    url: url + "/" + visibility.id,
+    method: "put",
+    data: visibility,
+    onSuccess: visibilityUpdated.type,
+  });
+
+
+
